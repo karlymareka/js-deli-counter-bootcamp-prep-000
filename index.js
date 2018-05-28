c066d6ae7d9466f98cd38dd179e32bd1e5836801
Karly Mareka <karly.mareka@gmail.com>
2018-05-28 19:25:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-      return ("The line is currently empty.");
+    return ("The line is currently empty.");
   }
   else {
     var katzDeliLine2 = [];
     for (var i = 0; i < katzDeliLine.length; i++) {
-      
-    }  
+        katzDeliLine2.push(`${i}. ${katzDeliLine[i]}`);
+    }
+    return katzDeliLine2;
   }
 }
