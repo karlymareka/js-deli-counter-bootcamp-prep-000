6e8a1c42981941eb5b75b71ce802f3e6be3b2f35
Karly Mareka <karly.mareka@gmail.com>
2018-05-28 19:30:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine) {
   else {
     var katzDeliLine2 = [];
     for (var i = 0; i < katzDeliLine.length; i++) {
-        katzDeliLine2.push(`${i + 1}. ${katzDeliLine[i]}`);
+        katzDeliLine2.push(`${i + 1}. ${katzDeliLine[i]} `);
     }
     return (`The line is currently: ${katzDeliLine2}`);
   }
