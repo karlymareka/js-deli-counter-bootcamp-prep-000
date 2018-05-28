4444bbf0a6c3dc7d66d2411202a2bb571b58bbf3
Karly Mareka <karly.mareka@gmail.com>
2018-05-28 19:10:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing(katzDeliLine) {
     return ("There is nobody waiting to be served!");
   }
   else {
-    return katzDeliLine[0];
+    return (`Currently serving ${katzDeliLine[0]}.`);
     katzDeliLine.shift();
   }
 }
