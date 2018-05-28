9dc89de8fc12140785bc36d9de1b095f7a77d20e
Karly Mareka <karly.mareka@gmail.com>
2018-05-28 18:52:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function takeANumber(katzDeliLine, nextPerson) {
-  return `Welcome, ${nextPerson}. You are number ${katzDeliLine.length + 1} in line`);
+  return `Welcome, ${nextPerson}. You are number ${katzDeliLine.length + 1} in line`;
 }
